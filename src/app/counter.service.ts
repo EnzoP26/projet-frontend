1301ec2a8af7ca6503422ee8bea320c3104283dd
QuentinGchrd <55576077+QuentinGchrd@users.noreply.github.com>
2019-11-07 20:30:14 +0100

    Update counter.service.ts

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -18,8 +18,9 @@ export class CounterService {
     return this.initialValue[position];
   }*/
    increment(id:number): Observable<Counter>{
-    this.httpClient.patch(this.url+id+".json",{"value" : 1}).subscribe();
-    return this.httpClient.get<Counter>(this.url+id+".json");
+    //this.httpClient.patch(this.url+id+".json",{"value" : 1}).subscribe();
+    //return this.httpClient.get<Counter>(this.url+id+".json");
+     return this.http.patch<Counter>(this.counterUrl + id + '.json', {});
   }
   decrement(position: number): number{
     this.initialValue[position]--;
